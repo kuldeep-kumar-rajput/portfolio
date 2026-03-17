d95bcaaff1623be9eb44a3a81b383ab9574e32a3
Kuldeep kumar <kuldeepk78079@gmail.com>
2026-03-17 07:35:51 +0530

    Update main.jsx

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -54,9 +54,9 @@ const Main = () => {
              </span>
              . I build responsive and modern web applications with clean UI.
               </p>
-            <div className="flex justify-center md:justify-start gap-4 mt-4">
+            <div className="flex justify-center md:justify-start gap-4 ">
               <Link to="/contact">
-                <button className="bg-amber-400 text-black px-8 py-3 rounded-full font-semibold shadow-lg hover:bg-amber-500 hover:scale-105 transition duration-300 mt-6">
+                <button className="bg-amber-400 text-black px-8 py-3 rounded-full font-semibold shadow-lg hover:bg-amber-500 hover:scale-105 transition duration-300 ">
                   Contact Me
                 </button>
               </Link>
