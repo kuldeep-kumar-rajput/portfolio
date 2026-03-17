8d6d2cf83940b6203c651693d8687c51512caef1
Kuldeep kumar <kuldeepk78079@gmail.com>
2026-03-17 07:32:41 +0530

    Update main.jsx

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -56,7 +56,7 @@ const Main = () => {
               </p>
             <div className="flex justify-center md:justify-start gap-4 mt-4">
               <Link to="/contact">
-                <button className="bg-amber-400 text-black px-8 py-3 rounded-full font-semibold shadow-lg hover:bg-amber-500 hover:scale-105 transition duration-300">
+                <button className="bg-amber-400 text-black px-8 py-3 rounded-full font-semibold shadow-lg hover:bg-amber-500 hover:scale-105 transition duration-300 mt-6">
                   Contact Me
                 </button>
               </Link>
